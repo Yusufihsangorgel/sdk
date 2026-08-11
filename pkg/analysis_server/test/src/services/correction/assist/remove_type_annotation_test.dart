39aead2cbd883b7cec58c63fd351967fc2c6fbf0
Yusuf İhsan Görgel <developeryusuf@icloud.com>
2026-08-11 09:18:30 +0000

    [analysis_server] Look through cascades for dot shorthand initializers
    Removing the declared type erases the context type a dot shorthand needs,
    so both producers write it back in front of the shorthand. Neither
    unwrapped a cascade first. `C<int> c = .new()..toString();` came out as
    `var c = .new()..toString();`, which no longer resolves.

--- pkg/analysis_server/test/src/services/correction/assist/remove_type_annotation_test.dart
+++ pkg/analysis_server/test/src/services/correction/assist/remove_type_annotation_test.dart
@@ -183,6 +183,24 @@ Set f() {
 ''');
   }
 
+  Future<void>
+  test_generic_instanceCreation_cascade_dotShorthandTarget() async {
+    await resolveTestCode('''
+C<int> f() {
+  final ^C<int> c = .new()..toString();
+  return c;
+}
+class C<T> {}
+''');
+    await assertHasAssist('''
+C<int> f() {
+  final c = C<int>.new()..toString();
+  return c;
+}
+class C<T> {}
+''');
+  }
+
   Future<void> test_generic_instanceCreation_withoutArguments() async {
     await resolveTestCode('''
 C<int> ^c = C();
